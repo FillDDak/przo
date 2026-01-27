12b72e99994487e031fe97c0e0f7f131b9f5b57b
FillDDak <cgr456@naver.com>
2026-01-27 10:56:50 +0900

    푸터, home.css 변경~

--- front-end/src/components/Footer.jsx
+++ front-end/src/components/Footer.jsx
@@ -14,7 +14,7 @@ const Footer = () => {
           <div className="footer__left">
             <img src={logoGreen} alt="PRZO Logo" className="footer__logo" />
             <div className="footer__social">
-              <a href="https://pf.kakao.com/_yourchannel" target="_blank" rel="noopener noreferrer">
+              <a href="https://open.kakao.com/o/sYCdK5og" target="_blank" rel="noopener noreferrer">
                 <img src={kakaotalkIcon} alt="kakaotalk" />
               </a>
               <a href="https://instagram.com/_yourprofile" target="_blank" rel="noopener noreferrer">
@@ -29,29 +29,34 @@ const Footer = () => {
             </div>
           </div>
 
-          <nav className="footer__nav">
-            <a href="#">사이트 이용 약관</a>
-            <a href="#">쿠키 정책</a>
-            <a href="#">개인정보처리방침</a>
-            <a href="#">많이 묻는 질문</a>
-            <a href="#">매장 안내</a>
-          </nav>
-
-          <div className="footer__contact">
-            <div className="footer__tel">프르조 상담문의 1670-2335</div>
-            <div className="footer__tel-sub">TEL:032-433-3348</div>
+          <div className="footer__center">
+            <nav className="footer__nav">
+              <a href="#">사이트 이용 약관</a>
+              <a href="#">쿠키 정책</a>
+              <a href="#">개인정보처리방침</a>
+              <a href="#">많이 묻는 질문</a>
+              <a href="#">매장 안내</a>
+            </nav>
+            <div className="footer__copyright">© 2025. 프르조 All Rights Reserved</div>
           </div>
-        </div>
 
-        <div className="footer__bottom">
-          <div className="footer__copyright">© 2025. 프르조 All Rights Reserved</div>
-          <div className="footer__info">
-            <span>대표: 김선미</span>
-            <span>인천 미추홀구 주안동 431-1</span>
-            <span>사업자등록번호: 653-98-00887</span>
-            <span>운영시간: 평일 09:00 ~ 18:00</span>
-            <span>E_mail:pestredzone@naver.com</span>
-            <span>FAX:032 511 3348</span>
+          <div className="footer__right">
+            <div className="footer__tel-wrapper">
+              <span className="footer__tel">프르조 상담문의 1670-2335</span>
+              <span className="footer__tel-sub">TEL: 032-433-3348</span>
+            </div>
+            <div className="footer__info">
+              <div className="footer__info-row">
+                <span>대표: 김선미</span>
+                <span>인천 계양구 마장로544번길 10</span>
+                <span>사업자 등록번호: 653-98-00887</span>
+              </div>
+              <div className="footer__info-row">
+                <span>운영시간: 평일 09:00 ~ 18:00</span>
+                <span>E_mail: pestredzone@naver.com</span>
+                <span>FAX: 032 511 3348</span>
+              </div>
+            </div>
           </div>
         </div>
       </div>
