6e821c6e2b198aea55fcf24dbff1d41d98fa616f
FillDDak <cgr456@naver.com>
2026-01-27 22:35:43 +0900

    home > Home.jsx 및 Home.css로 이름 변경, AdminLogin.jsx, css 생성

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -7,7 +7,7 @@ import "./index.css";
 import MainLayout from "./layouts/MainLayout";
 
 // 페이지
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 
 const router = createBrowserRouter([
