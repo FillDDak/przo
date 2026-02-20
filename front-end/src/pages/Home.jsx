740a9dcd9a6f5b8fedc0f243c780b1405c1a7150
FillDDak <cgr456@naver.com>
2026-02-20 22:01:08 +0900

    feat: 섹션 1 페이드-업 효과 추가

--- front-end/src/pages/Home.jsx
+++ front-end/src/pages/Home.jsx
@@ -594,15 +594,15 @@ const Home = () => {
       >
         <div className="home__content">
           <div className="home__section1-wrapper">
-            <p className="home__section1-subtitle">
+            <p className="home__section1-subtitle fade-up">
               방역 전문가들의 맞춤 솔루션
             </p>
-            <h1 className="home__section1-title">
+            <h1 className="home__section1-title fade-up fade-up-delay-1">
               당신의 소중한 공간을
               <br />
               안전하게 지켜드립니다
             </h1>
-            <button className="home__section1-btn" onClick={() => setIsModalOpen(true)}>무료 상담 문의</button>
+            <button className="home__section1-btn fade-up fade-up-delay-2" onClick={() => setIsModalOpen(true)}>무료 상담 문의</button>
           </div>
           <div className="home__section1-scroll-hint">
             <svg className="home__section1-scroll-hint__icon" width="28" height="28" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
